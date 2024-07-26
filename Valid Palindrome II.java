2f032fe26a0016325b3be9112c7219a1825afc55
Nandini Jain <113116163+Nandini26-prog@users.noreply.github.com>
2024-07-26 12:59:41 +0530

    Create Valid Palindrome II.java
    Two Pointers
    String
    Greedy

---
+++ Valid Palindrome II.java
@@ -0,0 +1,52 @@
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+ 
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+
+Input: s = "abc"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+     
+         while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // If characters at left and right don't match, try to skip one character
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true; // The string is a palindrome or can become one by removing one character
+    }
+    
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
